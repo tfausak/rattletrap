3189aa75a4eb213a675f3b8de18854aa47e81ea0
Taylor Fausak <taylor@fausak.me>
2016-10-02 17:29:27 -0500

    Remove size from text data type

--- library/Rattletrap/Text.hs
+++ library/Rattletrap/Text.hs
@@ -9,29 +9,24 @@ import qualified Data.ByteString.Lazy.Char8 as ByteString
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Encoding
 
-data Text = Text
-  { textSize :: Int32
-  , textValue :: Text.Text
+newtype Text = Text
+  { textValue :: Text.Text
   } deriving (Eq, Ord, Show)
 
 getText :: Binary.Get Text
 getText = do
   size <- getInt32
   bytes <- Binary.getLazyByteString (fromIntegral (int32Value size))
-  let value = Encoding.decodeUtf8 (ByteString.toStrict bytes)
-  pure Text {textSize = size, textValue = value}
+  let text = Encoding.decodeUtf8 (ByteString.toStrict bytes)
+  pure (Text text)
 
 putText :: Text -> Binary.Put
-putText text = do
-  putInt32 (textSize text)
-  let bytes = Encoding.encodeUtf8 (textValue text)
-  Binary.putByteString bytes
+putText (Text text) = do
+  putInt32 (Int32 (fromIntegral (Text.length text)))
+  Binary.putByteString (Encoding.encodeUtf8 text)
 
 stringToText :: String -> Text
-stringToText string =
-  let value = Text.snoc (Text.pack string) '\x00'
-      size = Int32 (fromIntegral (Text.length value))
-  in Text {textSize = size, textValue = value}
+stringToText string = Text (Text.snoc (Text.pack string) '\x00')
 
 textToString :: Text -> String
-textToString text = Text.unpack (Text.dropWhileEnd (== '\x00') (textValue text))
+textToString (Text text) = Text.unpack (Text.dropWhileEnd (== '\x00') text)
