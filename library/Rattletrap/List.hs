28028007daec42bf7a08c17f7fd46be6bacd8b64
Taylor Fausak <taylor@fausak.me>
2016-10-02 17:26:30 -0500

    Remove size from list data type

--- library/Rattletrap/List.hs
+++ library/Rattletrap/List.hs
@@ -5,18 +5,17 @@ import Rattletrap.Word32
 import qualified Control.Monad as Monad
 import qualified Data.Binary as Binary
 
-data List a = List
-  { listSize :: Word32
-  , listValue :: [a]
+newtype List a = List
+  { listValue :: [a]
   } deriving (Eq, Ord, Show)
 
 getList :: Binary.Get a -> Binary.Get (List a)
 getList getElement = do
   size <- getWord32
   elements <- Monad.replicateM (fromIntegral (word32Value size)) getElement
-  pure List {listSize = size, listValue = elements}
+  pure (List elements)
 
 putList :: (a -> Binary.Put) -> List a -> Binary.Put
-putList putElement list = do
-  putWord32 (listSize list)
-  mapM_ putElement (listValue list)
+putList putElement (List elements) = do
+  putWord32 (Word32 (fromIntegral (length elements)))
+  mapM_ putElement elements
