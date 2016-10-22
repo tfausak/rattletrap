6261d348b32f85a112a66bffe0abf36499efc431
Taylor Fausak <taylor@fausak.me>
2016-10-22 12:31:43 -0500

    Fix data formatting

--- library/Rattletrap/Data.hs
+++ library/Rattletrap/Data.hs
@@ -27,8 +27,7 @@ rawParentClasses =
   , ("TAGame.GameEvent_Season_TA", "TAGame.GameEvent_Soccar_TA")
   , ("TAGame.GameEvent_Soccar_TA", "TAGame.GameEvent_Team_TA")
   , ("TAGame.GameEvent_SoccarPrivate_TA", "TAGame.GameEvent_Soccar_TA")
-  , ( "TAGame.GameEvent_SoccarSplitscreen_TA"
-    , "TAGame.GameEvent_SoccarPrivate_TA")
+  , ("TAGame.GameEvent_SoccarSplitscreen_TA", "TAGame.GameEvent_SoccarPrivate_TA")
   , ("TAGame.GameEvent_TA", "Engine.ReplicationInfo")
   , ("TAGame.GameEvent_Team_TA", "TAGame.GameEvent_TA")
   , ("TAGame.GRI_TA", "ProjectX.GRI_X")
@@ -38,14 +37,11 @@ rawParentClasses =
   , ("TAGame.SpecialPickup_BallCarSpring_TA", "TAGame.SpecialPickup_Spring_TA")
   , ("TAGame.SpecialPickup_BallFreeze_TA", "TAGame.SpecialPickup_Targeted_TA")
   , ("TAGame.SpecialPickup_BallGravity_TA", "TAGame.SpecialPickup_TA")
-  , ( "TAGame.SpecialPickup_BallLasso_TA"
-    , "TAGame.SpecialPickup_GrapplingHook_TA")
+  , ("TAGame.SpecialPickup_BallLasso_TA", "TAGame.SpecialPickup_GrapplingHook_TA")
   , ("TAGame.SpecialPickup_BallVelcro_TA", "TAGame.SpecialPickup_TA")
   , ("TAGame.SpecialPickup_Batarang_TA", "TAGame.SpecialPickup_BallLasso_TA")
-  , ( "TAGame.SpecialPickup_BoostOverride_TA"
-    , "TAGame.SpecialPickup_Targeted_TA")
-  , ( "TAGame.SpecialPickup_GrapplingHook_TA"
-    , "TAGame.SpecialPickup_Targeted_TA")
+  , ("TAGame.SpecialPickup_BoostOverride_TA", "TAGame.SpecialPickup_Targeted_TA")
+  , ("TAGame.SpecialPickup_GrapplingHook_TA", "TAGame.SpecialPickup_Targeted_TA")
   , ("TAGame.SpecialPickup_HitForce_TA", "TAGame.SpecialPickup_TA")
   , ("TAGame.SpecialPickup_Spring_TA", "TAGame.SpecialPickup_Targeted_TA")
   , ("TAGame.SpecialPickup_Swapper_TA", "TAGame.SpecialPickup_Targeted_TA")
@@ -94,7 +90,10 @@ rawClassesWithLocation =
 
 rawClassesWithRotation :: [String]
 rawClassesWithRotation =
-  ["TAGame.Ball_TA", "TAGame.Car_Season_TA", "TAGame.Car_TA"]
+  [ "TAGame.Ball_TA"
+  , "TAGame.Car_Season_TA"
+  , "TAGame.Car_TA"
+  ]
 
 rawObjectClasses :: [(String, String)]
 rawObjectClasses =
@@ -103,69 +102,41 @@ rawObjectClasses =
   , ("Archetypes.Ball.Ball_Puck", "TAGame.Ball_TA")
   , ("Archetypes.Ball.CubeBall", "TAGame.Ball_TA")
   , ("Archetypes.Car.Car_Default", "TAGame.Car_TA")
-  , ( "Archetypes.CarComponents.CarComponent_Boost"
-    , "TAGame.CarComponent_Boost_TA")
-  , ( "Archetypes.CarComponents.CarComponent_Dodge"
-    , "TAGame.CarComponent_Dodge_TA")
-  , ( "Archetypes.CarComponents.CarComponent_DoubleJump"
-    , "TAGame.CarComponent_DoubleJump_TA")
-  , ( "Archetypes.CarComponents.CarComponent_FlipCar"
-    , "TAGame.CarComponent_FlipCar_TA")
-  , ( "Archetypes.CarComponents.CarComponent_Jump"
-    , "TAGame.CarComponent_Jump_TA")
+  , ("Archetypes.CarComponents.CarComponent_Boost", "TAGame.CarComponent_Boost_TA")
+  , ("Archetypes.CarComponents.CarComponent_Dodge", "TAGame.CarComponent_Dodge_TA")
+  , ("Archetypes.CarComponents.CarComponent_DoubleJump", "TAGame.CarComponent_DoubleJump_TA")
+  , ("Archetypes.CarComponents.CarComponent_FlipCar", "TAGame.CarComponent_FlipCar_TA")
+  , ("Archetypes.CarComponents.CarComponent_Jump", "TAGame.CarComponent_Jump_TA")
   , ("Archetypes.GameEvent.GameEvent_Basketball", "TAGame.GameEvent_Soccar_TA")
-  , ( "Archetypes.GameEvent.GameEvent_BasketballPrivate"
-    , "TAGame.GameEvent_SoccarPrivate_TA")
-  , ( "Archetypes.GameEvent.GameEvent_BasketballSplitscreen"
-    , "TAGame.GameEvent_SoccarSplitscreen_TA")
-  , ( "Archetypes.GameEvent.GameEvent_HockeyPrivate"
-    , "TAGame.GameEvent_SoccarPrivate_TA")
-  , ( "Archetypes.GameEvent.GameEvent_HockeySplitscreen"
-    , "TAGame.GameEvent_SoccarSplitscreen_TA")
+  , ("Archetypes.GameEvent.GameEvent_BasketballPrivate", "TAGame.GameEvent_SoccarPrivate_TA")
+  , ("Archetypes.GameEvent.GameEvent_BasketballSplitscreen", "TAGame.GameEvent_SoccarSplitscreen_TA")
+  , ("Archetypes.GameEvent.GameEvent_HockeyPrivate", "TAGame.GameEvent_SoccarPrivate_TA")
+  , ("Archetypes.GameEvent.GameEvent_HockeySplitscreen", "TAGame.GameEvent_SoccarSplitscreen_TA")
   , ("Archetypes.GameEvent.GameEvent_Items", "TAGame.GameEvent_Soccar_TA")
   , ("Archetypes.GameEvent.GameEvent_Season:CarArchetype", "TAGame.Car_TA")
   , ("Archetypes.GameEvent.GameEvent_Season", "TAGame.GameEvent_Season_TA")
   , ("Archetypes.GameEvent.GameEvent_Soccar", "TAGame.GameEvent_Soccar_TA")
-  , ( "Archetypes.GameEvent.GameEvent_SoccarPrivate"
-    , "TAGame.GameEvent_SoccarPrivate_TA")
-  , ( "Archetypes.GameEvent.GameEvent_SoccarSplitscreen"
-    , "TAGame.GameEvent_SoccarSplitscreen_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_BallFreeze"
-    , "TAGame.SpecialPickup_BallFreeze_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_BallGrapplingHook"
-    , "TAGame.SpecialPickup_GrapplingHook_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_BallLasso"
-    , "TAGame.SpecialPickup_BallLasso_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_BallSpring"
-    , "TAGame.SpecialPickup_BallCarSpring_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_BallVelcro"
-    , "TAGame.SpecialPickup_BallVelcro_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_Batarang"
-    , "TAGame.SpecialPickup_Batarang_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_BoostOverride"
-    , "TAGame.SpecialPickup_BoostOverride_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_CarSpring"
-    , "TAGame.SpecialPickup_BallCarSpring_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_GravityWell"
-    , "TAGame.SpecialPickup_BallGravity_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_StrongHit"
-    , "TAGame.SpecialPickup_HitForce_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_Swapper"
-    , "TAGame.SpecialPickup_Swapper_TA")
-  , ( "Archetypes.SpecialPickups.SpecialPickup_Tornado"
-    , "TAGame.SpecialPickup_Tornado_TA")
+  , ("Archetypes.GameEvent.GameEvent_SoccarPrivate", "TAGame.GameEvent_SoccarPrivate_TA")
+  , ("Archetypes.GameEvent.GameEvent_SoccarSplitscreen", "TAGame.GameEvent_SoccarSplitscreen_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_BallFreeze", "TAGame.SpecialPickup_BallFreeze_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_BallGrapplingHook", "TAGame.SpecialPickup_GrapplingHook_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_BallLasso", "TAGame.SpecialPickup_BallLasso_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_BallSpring", "TAGame.SpecialPickup_BallCarSpring_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_BallVelcro", "TAGame.SpecialPickup_BallVelcro_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_Batarang", "TAGame.SpecialPickup_Batarang_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_BoostOverride", "TAGame.SpecialPickup_BoostOverride_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_CarSpring", "TAGame.SpecialPickup_BallCarSpring_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_GravityWell", "TAGame.SpecialPickup_BallGravity_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_StrongHit", "TAGame.SpecialPickup_HitForce_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_Swapper", "TAGame.SpecialPickup_Swapper_TA")
+  , ("Archetypes.SpecialPickups.SpecialPickup_Tornado", "TAGame.SpecialPickup_Tornado_TA")
   , ("Archetypes.Teams.Team0", "TAGame.Team_Soccar_TA")
   , ("Archetypes.Teams.Team1", "TAGame.Team_Soccar_TA")
-  , ( "GameInfo_Basketball.GameInfo.GameInfo_Basketball:GameReplicationInfoArchetype"
-    , "TAGame.GRI_TA")
-  , ( "Gameinfo_Hockey.GameInfo.Gameinfo_Hockey:GameReplicationInfoArchetype"
-    , "TAGame.GRI_TA")
-  , ( "GameInfo_Items.GameInfo.GameInfo_Items:GameReplicationInfoArchetype"
-    , "TAGame.GRI_TA")
-  , ( "GameInfo_Season.GameInfo.GameInfo_Season:GameReplicationInfoArchetype"
-    , "TAGame.GRI_TA")
-  , ( "GameInfo_Soccar.GameInfo.GameInfo_Soccar:GameReplicationInfoArchetype"
-    , "TAGame.GRI_TA")
+  , ("GameInfo_Basketball.GameInfo.GameInfo_Basketball:GameReplicationInfoArchetype", "TAGame.GRI_TA")
+  , ("Gameinfo_Hockey.GameInfo.Gameinfo_Hockey:GameReplicationInfoArchetype", "TAGame.GRI_TA")
+  , ("GameInfo_Items.GameInfo.GameInfo_Items:GameReplicationInfoArchetype", "TAGame.GRI_TA")
+  , ("GameInfo_Season.GameInfo.GameInfo_Season:GameReplicationInfoArchetype", "TAGame.GRI_TA")
+  , ("GameInfo_Soccar.GameInfo.GameInfo_Soccar:GameReplicationInfoArchetype", "TAGame.GRI_TA")
   , ("TAGame.CameraSettingsActor_TA:PRI", "TAGame.CameraSettingsActor_TA")
   , ("TAGame.Default__CameraSettingsActor_TA", "TAGame.CameraSettingsActor_TA")
   , ("TAGame.Default__PRI_TA", "TAGame.PRI_TA")
@@ -173,6 +144,5 @@ rawObjectClasses =
   , ("TheWorld:PersistentLevel.CrowdActor_TA", "TAGame.CrowdActor_TA")
   , ("TheWorld:PersistentLevel.CrowdManager_TA", "TAGame.CrowdManager_TA")
   , ("TheWorld:PersistentLevel.InMapScoreboard_TA", "TAGame.InMapScoreboard_TA")
-  , ( "TheWorld:PersistentLevel.VehiclePickup_Boost_TA"
-    , "TAGame.VehiclePickup_Boost_TA")
+  , ("TheWorld:PersistentLevel.VehiclePickup_Boost_TA", "TAGame.VehiclePickup_Boost_TA")
   ]
