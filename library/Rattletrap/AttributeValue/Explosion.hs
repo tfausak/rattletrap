0616fc156ac11047aa4aa833d23dc4e45a71a4f3
Taylor Fausak <taylor@fausak.me>
2016-10-27 09:28:45 -0500

    Explosions always have actors

--- library/Rattletrap/AttributeValue/Explosion.hs
+++ library/Rattletrap/AttributeValue/Explosion.hs
@@ -7,27 +7,19 @@ import qualified Data.Binary.Bits.Get as BinaryBit
 import qualified Data.Binary.Bits.Put as BinaryBit
 
 data ExplosionAttributeValue = ExplosionAttributeValue
-  { explosionAttributeValueMaybeActorId :: Maybe Int32
+  { explosionAttributeValueActorId :: Int32
   , explosionAttributeValueLocation :: Vector
   } deriving (Eq, Ord, Show)
 
 getExplosionAttributeValue :: BinaryBit.BitGet ExplosionAttributeValue
 getExplosionAttributeValue = do
-  actorless <- BinaryBit.getBool
-  maybeActorId <-
-    if actorless
-      then pure Nothing
-      else do
-        actorId <- getInt32Bits
-        pure (Just actorId)
+  False <- BinaryBit.getBool
+  actorId <- getInt32Bits
   location <- getVector
-  pure (ExplosionAttributeValue maybeActorId location)
+  pure (ExplosionAttributeValue actorId location)
 
 putExplosionAttributeValue :: ExplosionAttributeValue -> BinaryBit.BitPut ()
 putExplosionAttributeValue explosionAttributeValue = do
-  case explosionAttributeValueMaybeActorId explosionAttributeValue of
-    Nothing -> BinaryBit.putBool True
-    Just actorId -> do
-      BinaryBit.putBool False
-      putInt32Bits actorId
+  BinaryBit.putBool False
+  putInt32Bits (explosionAttributeValueActorId explosionAttributeValue)
   putVector (explosionAttributeValueLocation explosionAttributeValue)
