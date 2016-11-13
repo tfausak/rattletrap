0bdaeeffb1db81834c388dc27ee2a55a3ad59b7b
Taylor Fausak <taylor@fausak.me>
2016-11-13 16:07:44 -0600

    Remove lint test suite

--- test/Lint.hs
+++
@@ -1,9 +0,0 @@
-import qualified Language.Haskell.HLint3 as HLint
-import qualified System.Exit as Exit
-
-main :: IO ()
-main = do
-  ideas <- HLint.hlint ["lint", "--cross", "."]
-  if null ideas
-    then Exit.exitSuccess
-    else Exit.exitFailure
