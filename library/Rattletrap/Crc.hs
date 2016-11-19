b956de8d94774a204a0cb5a81502661b028286e1
Taylor Fausak <taylor@fausak.me>
2016-11-19 09:04:13 -0600

    Document Rattletrap.Crc

--- library/Rattletrap/Crc.hs
+++ library/Rattletrap/Crc.hs
@@ -7,6 +7,12 @@ import qualified Data.ByteString.Lazy as ByteString
 import qualified Data.Vector.Unboxed as Vector
 import qualified Data.Word as Word
 
+-- | Computes the CRC32 of some bytes. This is done to ensure that the bytes
+-- are valid before trying to parse them.
+--
+-- @
+-- getCrc32 ('Data.ByteString.Lazy.pack' [0x00])
+-- @
 getCrc32 :: ByteString.ByteString -> Word.Word32
 getCrc32 bytes = do
   let update = crc32Update crc32Table
