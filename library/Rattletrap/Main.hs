35adc4434f13e6c7296ee44912d8ba5082b96526
Taylor Fausak <taylor@fausak.me>
2016-11-18 19:41:54 -0600

    Document Rattletrap.Main

--- library/Rattletrap/Main.hs
+++ library/Rattletrap/Main.hs
@@ -11,11 +11,23 @@ import qualified Data.ByteString.Lazy as ByteString
 import qualified Data.Version as Version
 import qualified System.Environment as Environment
 
+-- | Gets command-line arguments and passes them to 'mainWithArgs'.
 main :: IO ()
 main = do
   args <- Environment.getArgs
   mainWithArgs args
 
+-- | Runs the main command-line interface. Can be used in one of three modes:
+--
+-- 1. @mainWithArgs ["version"]@: Prints out the version number and exits.
+--    Mostly useful for debugging.
+-- 2. @mainWithArgs ["decode"]@: Parses a raw replay into JSON. By default
+--    this reads from STDIN and writes to STDOUT. To read from a file, pass a
+--    second argument like @mainWithArgs ["decode", "input.replay"]@. To write
+--    to a file, pass a third argument like
+--    @mainWithArgs ["decode", "input.replay", "output.json"]@.
+-- 3. @mainWithArgs ["encode"]@: Generates a raw replay from JSON. The
+--    handling of input and output is the same as decoding.
 mainWithArgs :: [String] -> IO ()
 mainWithArgs args =
   case args of
