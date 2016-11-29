83eaf09e7678ab41e64a3d054c1ce3900d8fe006
Taylor Fausak <taylor@fausak.me>
2016-11-29 08:19:08 -0600

    Store splitscreen IDs as numbers rather than bytes
    This sort of ties into #16.

--- library/Rattletrap/RemoteId.hs
+++ library/Rattletrap/RemoteId.hs
@@ -9,7 +9,7 @@ import qualified Data.Word as Word
 
 data RemoteId
   = PlayStationId [Word.Word8]
-  | SplitscreenId [Word.Word8]
+  | SplitscreenId Word.Word32
   | SteamId Word64
   | XboxId Word64
   deriving (Eq, Ord, Show)
@@ -18,8 +18,8 @@ getRemoteId :: Word8 -> BinaryBit.BitGet RemoteId
 getRemoteId systemId =
   case word8Value systemId of
     0 -> do
-      bytes <- BinaryBit.getLazyByteString 3
-      pure (SplitscreenId (ByteString.unpack bytes))
+      word24 <- BinaryBit.getWord32be 24
+      pure (SplitscreenId word24)
     1 -> do
       word64 <- getWord64Bits
       pure (SteamId word64)
@@ -36,7 +36,6 @@ putRemoteId remoteId =
   case remoteId of
     PlayStationId bytes ->
       BinaryBit.putByteString (ByteString.toStrict (ByteString.pack bytes))
-    SplitscreenId bytes ->
-      BinaryBit.putByteString (ByteString.toStrict (ByteString.pack bytes))
+    SplitscreenId word24 -> BinaryBit.putWord32be 24 word24
     SteamId word64 -> putWord64Bits word64
     XboxId word64 -> putWord64Bits word64
