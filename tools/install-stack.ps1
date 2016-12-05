2a323b1d8cf1eef2eb2080d9492726e6b29d4ac1
Taylor Fausak <taylor@fausak.me>
2016-12-05 09:08:36 -0600

    Download Stack from GitHub

--- tools/install-stack.ps1
+++ tools/install-stack.ps1
@@ -1,5 +1,5 @@
 if (Test-Path c:\bin\stack.exe) {} else {
-  curl -OutFile stack.zip -Uri https://www.stackage.org/stack/windows-x86_64
+  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v1.2.0/stack-1.2.0-windows-x86_64.zip
   7z x stack.zip stack.exe
   mkdir c:\bin
   mv stack.exe c:\bin
