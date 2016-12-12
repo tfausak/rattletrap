5bf49fa605cffd9365a23ec98af3789b4e603e6a
Taylor Fausak <taylor@fausak.me>
2016-12-12 08:40:24 -0600

    Add helpers for working with handles

--- source/library/Rattletrap/Helper.hs
+++ source/library/Rattletrap/Helper.hs
@@ -7,25 +7,37 @@ import qualified Data.Aeson as Aeson
 import qualified Data.Binary.Get as Binary
 import qualified Data.Binary.Put as Binary
 import qualified Data.ByteString.Lazy as ByteString
+import qualified System.IO as IO
 
 encodeJson :: Replay -> ByteString.ByteString
 encodeJson replay = Aeson.encode replay
 
 encodeJsonFile :: Replay -> FilePath -> IO ()
-encodeJsonFile replay file = ByteString.writeFile file (encodeJson replay)
+encodeJsonFile replay file =
+  IO.withBinaryFile file IO.WriteMode (encodeJsonHandle replay)
+
+encodeJsonHandle :: Replay -> IO.Handle -> IO ()
+encodeJsonHandle replay handle = ByteString.hPut handle (encodeJson replay)
 
 encodeReplay :: Replay -> ByteString.ByteString
 encodeReplay replay = Binary.runPut (putReplay replay)
 
 encodeReplayFile :: Replay -> FilePath -> IO ()
-encodeReplayFile replay file = ByteString.writeFile file (encodeReplay replay)
+encodeReplayFile replay file =
+  IO.withBinaryFile file IO.WriteMode (encodeReplayHandle replay)
+
+encodeReplayHandle :: Replay -> IO.Handle -> IO ()
+encodeReplayHandle replay handle = ByteString.hPut handle (encodeReplay replay)
 
 decodeJson :: ByteString.ByteString -> Either String Replay
 decodeJson contents = Aeson.eitherDecode contents
 
 decodeJsonFile :: FilePath -> IO (Either String Replay)
-decodeJsonFile file = do
-  contents <- ByteString.readFile file
+decodeJsonFile file = IO.withBinaryFile file IO.ReadMode decodeJsonHandle
+
+decodeJsonHandle :: IO.Handle -> IO (Either String Replay)
+decodeJsonHandle handle = do
+  contents <- ByteString.hGetContents handle
   pure (decodeJson contents)
 
 decodeReplay :: ByteString.ByteString -> Either String Replay
@@ -35,6 +47,9 @@ decodeReplay contents =
     Right (_, _, replay) -> pure replay
 
 decodeReplayFile :: FilePath -> IO (Either String Replay)
-decodeReplayFile file = do
-  contents <- ByteString.readFile file
+decodeReplayFile file = IO.withBinaryFile file IO.ReadMode decodeReplayHandle
+
+decodeReplayHandle :: IO.Handle -> IO (Either String Replay)
+decodeReplayHandle handle = do
+  contents <- ByteString.hGetContents handle
   pure (decodeReplay contents)
