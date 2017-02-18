0e743ad267cdf2c4e337b0bca586ccdc845d5319
Taylor Fausak <taylor@fausak.me>
2017-02-18 10:19:02 -0600

    Store list values in a vector

--- source/library/Rattletrap/Primitive/List.hs
+++ source/library/Rattletrap/Primitive/List.hs
@@ -2,17 +2,20 @@ module Rattletrap.Primitive.List where
 
 import Rattletrap.Primitive.Word32
 
-import qualified Control.Monad as Monad
 import qualified Data.Binary as Binary
+import qualified Data.Vector as Vector
 
 newtype List a = List
-  { listValue :: [a]
+  { listVector :: Vector.Vector a
   } deriving (Eq, Show)
+
+listValue :: List a -> [a]
+listValue list = Vector.toList (listVector list)
 
 getList :: Binary.Get a -> Binary.Get (List a)
 getList getElement = do
   size <- getWord32
-  elements <- Monad.replicateM (fromIntegral (word32Value size)) getElement
+  elements <- Vector.replicateM (fromIntegral (word32Value size)) getElement
   pure (List elements)
 
 putList :: (a -> Binary.Put) -> List a -> Binary.Put
