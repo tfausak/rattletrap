54f89e79ab399bfdb49e78ac1b8238df0ca0fb76
Taylor Fausak <taylor@fausak.me>
2017-02-21 07:38:31 -0600

    Format code with hindent
    It's not exactly an improvement, but I'm trying to be consistent here.

--- source/library/Rattletrap/ActorMap.hs
+++ source/library/Rattletrap/ActorMap.hs
@@ -6,10 +6,16 @@ import qualified Data.Map.Strict as Map
 
 type ActorMap = Map.Map CompressedWord Word32
 
-{-# DEPRECATED makeActorMap "use Data.Map.Strict.empty" #-}
+{-# DEPRECATED
+makeActorMap "use Data.Map.Strict.empty"
+ #-}
+
 makeActorMap :: ActorMap
 makeActorMap = Map.empty
 
-{-# DEPRECATED updateActorMap "use Data.Map.Strict.insert" #-}
+{-# DEPRECATED
+updateActorMap "use Data.Map.Strict.insert"
+ #-}
+
 updateActorMap :: CompressedWord -> Word32 -> ActorMap -> ActorMap
 updateActorMap = Map.insert
