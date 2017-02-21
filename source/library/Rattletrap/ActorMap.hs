46f44e770f58160b4a496667b475c6f89c72c88e
Taylor Fausak <taylor@fausak.me>
2017-02-21 07:35:00 -0600

    Mark actor map functions as deprecated

--- source/library/Rattletrap/ActorMap.hs
+++ source/library/Rattletrap/ActorMap.hs
@@ -6,8 +6,10 @@ import qualified Data.Map.Strict as Map
 
 type ActorMap = Map.Map CompressedWord Word32
 
+{-# DEPRECATED makeActorMap "use Data.Map.Strict.empty" #-}
 makeActorMap :: ActorMap
 makeActorMap = Map.empty
 
+{-# DEPRECATED updateActorMap "use Data.Map.Strict.insert" #-}
 updateActorMap :: CompressedWord -> Word32 -> ActorMap -> ActorMap
 updateActorMap = Map.insert
