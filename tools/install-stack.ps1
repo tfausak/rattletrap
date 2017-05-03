bfc0b7939d867fef12317f20313316d204182e19
Taylor Fausak <taylor@fausak.me>
2017-05-03 08:22:09 -0500

    Upgrade to Stack 1.4.0

--- tools/install-stack.ps1
+++ tools/install-stack.ps1
@@ -1,5 +1,5 @@
 if (Test-Path c:\bin\stack.exe) {} else {
-  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v1.3.0/stack-1.3.0-windows-x86_64.zip
+  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v1.4.0/stack-1.4.0-windows-x86_64.zip
   7z x stack.zip stack.exe
   mkdir c:\bin
   mv stack.exe c:\bin
