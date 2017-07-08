05906faf3a06d0425c25f6930146da895c293b8b
Taylor Fausak <taylor@fausak.me>
2017-07-08 16:42:38 -0500

    Handle new online loadouts

--- library/Rattletrap/Attribute/LoadoutOnline.hs
+++ library/Rattletrap/Attribute/LoadoutOnline.hs
@@ -7,18 +7,27 @@ import qualified Data.Binary.Bits.Get as BinaryBit
 import qualified Data.Binary.Bits.Put as BinaryBit
 
 newtype LoadoutOnlineAttribute = LoadoutOnlineAttribute
-  { loadoutAttributeValue :: [[(Word32, CompressedWord)]]
+  { loadoutAttributeValue :: [[(Word32, Either CompressedWord Word32)]]
   } deriving (Eq, Ord, Show)
 
-getLoadoutOnlineAttribute :: BinaryBit.BitGet LoadoutOnlineAttribute
-getLoadoutOnlineAttribute = do
+getLoadoutOnlineAttribute ::
+     (Int, Int) -> BinaryBit.BitGet LoadoutOnlineAttribute
+getLoadoutOnlineAttribute version = do
   let getOuter = do
         innerSize <- getWord8Bits
         Monad.replicateM (fromIntegral (word8Value innerSize)) getInner
       getInner = do
         x <- getWord32Bits
-        y <- getCompressedWord 27
+        y <- getY version
         pure (x, y)
+      getY (major, minor) =
+        if major >= 868 && minor >= 18
+          then do
+            y <- getWord32Bits
+            pure (Right y)
+          else do
+            y <- getCompressedWord 27
+            pure (Left y)
   size <- getWord8Bits
   values <- Monad.replicateM (fromIntegral (word8Value size)) getOuter
   pure (LoadoutOnlineAttribute values)
@@ -30,7 +39,11 @@ putLoadoutOnlineAttribute loadoutAttribute = do
         mapM_ putInner xs
       putInner (x, y) = do
         putWord32Bits x
-        putCompressedWord y
+        putY y
+      putY y =
+        case y of
+          Left l -> putCompressedWord l
+          Right r -> putWord32Bits r
       value = loadoutAttributeValue loadoutAttribute
   putWord8Bits (Word8 (fromIntegral (length value)))
   mapM_ putOuter value
