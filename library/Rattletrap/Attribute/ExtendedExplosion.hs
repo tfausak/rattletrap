618516b8204a1e72ec3818086e39b2863830d324
Taylor Fausak <taylor@fausak.me>
2017-07-09 12:21:53 -0500

    Parse the correct number of bits for extended explosions

--- library/Rattletrap/Attribute/ExtendedExplosion.hs
+++ library/Rattletrap/Attribute/ExtendedExplosion.hs
@@ -1,18 +1,22 @@
 module Rattletrap.Attribute.ExtendedExplosion where
 
+import Rattletrap.Primitive
+
+import qualified Control.Monad as Monad
 import qualified Data.Binary.Bits.Get as BinaryBit
 import qualified Data.Binary.Bits.Put as BinaryBit
 
-data ExtendedExplosionAttribute = ExtendedExplosionAttribute
-  {
+newtype ExtendedExplosionAttribute = ExtendedExplosionAttribute
+  { extendedExplosionAttributeUnknown :: [Word8]
+  -- ^ Warning: This field should be considered experimental and could change
+  -- at any time!
   } deriving (Eq, Ord, Show)
 
 getExtendedExplosionAttribute :: BinaryBit.BitGet ExtendedExplosionAttribute
 getExtendedExplosionAttribute = do
-  -- TODO
-  pure ExtendedExplosionAttribute
+  unknown <- Monad.replicateM 14 getWord8Bits
+  pure (ExtendedExplosionAttribute unknown)
 
 putExtendedExplosionAttribute :: ExtendedExplosionAttribute -> BinaryBit.BitPut ()
-putExtendedExplosionAttribute _ = do
-  -- TODO
-  pure ()
+putExtendedExplosionAttribute extendedExplosionAttribute = do
+  mapM_ putWord8Bits (extendedExplosionAttributeUnknown extendedExplosionAttribute)
