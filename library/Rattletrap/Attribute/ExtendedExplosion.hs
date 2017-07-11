e994525d7cd9f2d689c997a373626cdb43e148e6
Taylor Fausak <taylor@fausak.me>
2017-07-11 08:06:17 -0500

    Improve parsing of extended explosion data
    This fixes #45.

--- library/Rattletrap/Attribute/ExtendedExplosion.hs
+++ library/Rattletrap/Attribute/ExtendedExplosion.hs
@@ -2,21 +2,31 @@ module Rattletrap.Attribute.ExtendedExplosion where
 
 import Rattletrap.Primitive
 
-import qualified Control.Monad as Monad
 import qualified Data.Binary.Bits.Get as BinaryBit
 import qualified Data.Binary.Bits.Put as BinaryBit
 
-newtype ExtendedExplosionAttribute = ExtendedExplosionAttribute
-  { extendedExplosionAttributeUnknown :: [Word8]
-  -- ^ Warning: This field should be considered experimental and could change
-  -- at any time!
+data ExtendedExplosionAttribute = ExtendedExplosionAttribute
+  { extendedExplosionAttributeActorId :: Int32
+  , extendedExplosionAttributeLocation :: Vector
+  , extendedExplosionAttributeUnknown1 :: Bool
+  , extendedExplosionAttributeUnknown2 :: Int32
   } deriving (Eq, Ord, Show)
 
 getExtendedExplosionAttribute :: BinaryBit.BitGet ExtendedExplosionAttribute
 getExtendedExplosionAttribute = do
-  unknown <- Monad.replicateM 14 getWord8Bits
-  pure (ExtendedExplosionAttribute unknown)
+  False <- BinaryBit.getBool
+  actorId <- getInt32Bits
+  location <- getVector
+  unknown1 <- BinaryBit.getBool
+  unknown2 <- getInt32Bits
+  pure (ExtendedExplosionAttribute actorId location unknown1 unknown2)
 
-putExtendedExplosionAttribute :: ExtendedExplosionAttribute -> BinaryBit.BitPut ()
+putExtendedExplosionAttribute ::
+     ExtendedExplosionAttribute -> BinaryBit.BitPut ()
 putExtendedExplosionAttribute extendedExplosionAttribute = do
-  mapM_ putWord8Bits (extendedExplosionAttributeUnknown extendedExplosionAttribute)
+  BinaryBit.putBool False
+  putInt32Bits (extendedExplosionAttributeActorId extendedExplosionAttribute)
+  putVector (extendedExplosionAttributeLocation extendedExplosionAttribute)
+  BinaryBit.putBool
+    (extendedExplosionAttributeUnknown1 extendedExplosionAttribute)
+  putInt32Bits (extendedExplosionAttributeUnknown2 extendedExplosionAttribute)
