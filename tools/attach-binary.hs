6b21587342455065deea0e6bdcba0c7b3b47f788
Taylor Fausak <taylor@fausak.me>
2017-12-15 12:17:43 -0600

    Exit earlier if tag does not exist

--- tools/attach-binary.hs
+++ tools/attach-binary.hs
@@ -5,17 +5,17 @@ import System.Process (callProcess)
 
 main :: IO ()
 main = do
-  token <- getEnv "GITHUB_TOKEN"
   appveyor <- lookupEnv "APPVEYOR"
   let windows = appveyor == Just "True"
+  maybeTag <- lookupEnv
+    (if windows then "APPVEYOR_REPO_TAG_NAME" else "TRAVIS_TAG")
+  tag <- maybe exitSuccess pure maybeTag
+  token <- getEnv "GITHUB_TOKEN"
   repo <- getEnv (if windows then "APPVEYOR_REPO_NAME" else "TRAVIS_REPO_SLUG")
   let
     executable = case break (== '/') repo of
       (_, '/':x) -> x
       (x, _) -> x
-  maybeTag <- lookupEnv
-    (if windows then "APPVEYOR_REPO_TAG_NAME" else "TRAVIS_TAG")
-  tag <- maybe exitSuccess pure maybeTag
   os <- if windows then pure "windows" else getEnv "TRAVIS_OS_NAME"
   callProcess "stack" ["build", "--copy-bins", "--local-bin-path", "."]
   callProcess
