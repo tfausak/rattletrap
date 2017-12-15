9557513903442a2b94d5104bc5d8a715d722b82a
Taylor Fausak <taylor@fausak.me>
2017-12-15 07:48:25 -0600

    Install github-release binary

---
+++ tools/install-github-release.ps1
@@ -0,0 +1,8 @@
+if (Test-Path c:\bin\github-release.exe) {} else {
+  curl -OutFile github-release.zip -Uri https://github.com/tfausak/github-release/releases/download/1.1.0/github-release-1.1.0-windows.zip
+  7z x github-release.zip github-release.exe
+  mkdir c:\bin
+  mv github-release.exe c:\bin
+}
+
+github-release version
