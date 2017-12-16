f6b17cd39c11b6c5b68a6cc599678854f498c08e
Taylor Fausak <taylor@fausak.me>
2017-12-15 18:40:15 -0600

    Update decoding of attributes

--- library/Rattletrap/Decode/Attribute.hs
+++ library/Rattletrap/Decode/Attribute.hs
@@ -5,49 +5,69 @@ module Rattletrap.Decode.Attribute
 
 import Data.Semigroup ((<>))
 import Rattletrap.Decode.AttributeValue
+import Rattletrap.Decode.Common
 import Rattletrap.Decode.CompressedWord
 import Rattletrap.Type.Attribute
 import Rattletrap.Type.ClassAttributeMap
+import Rattletrap.Type.Common
 import Rattletrap.Type.CompressedWord
+import Rattletrap.Type.Str
 import Rattletrap.Type.Word32le
 
-import qualified Data.Binary.Bits.Get as BinaryBit
-import qualified Data.Map as Map
+import qualified Data.Binary.Bits.Get as BinaryBits
 
 getAttributes
   :: (Int, Int, Int)
   -> ClassAttributeMap
-  -> Map.Map CompressedWord Word32le
+  -> Map CompressedWord Word32le
   -> CompressedWord
-  -> BinaryBit.BitGet [Attribute]
-getAttributes version classAttributeMap actorMap actorId = do
-  hasAttribute <- BinaryBit.getBool
-  if not hasAttribute
-    then pure []
-    else do
-      attribute <- getAttribute version classAttributeMap actorMap actorId
-      attributes <- getAttributes version classAttributeMap actorMap actorId
-      pure (attribute : attributes)
+  -> DecodeBits [Attribute]
+getAttributes version classes actors actor = do
+  hasAttribute <- BinaryBits.getBool
+  if hasAttribute
+    then
+      (:)
+      <$> getAttribute version classes actors actor
+      <*> getAttributes version classes actors actor
+    else pure []
 
 getAttribute
   :: (Int, Int, Int)
   -> ClassAttributeMap
-  -> Map.Map CompressedWord Word32le
+  -> Map CompressedWord Word32le
   -> CompressedWord
-  -> BinaryBit.BitGet Attribute
-getAttribute version classAttributeMap actorMap actorId =
-  case getAttributeMap classAttributeMap actorMap actorId of
-    Nothing -> fail ("could not get attribute map for " <> show actorId)
-    Just attributeMap -> case getAttributeIdLimit attributeMap of
-      Nothing ->
-        fail ("could not get attribute ID limit for " <> show actorId)
-      Just limit -> do
-        id_ <- getCompressedWord limit
-        case getAttributeName classAttributeMap attributeMap id_ of
-          Nothing -> fail ("could not get attribute name for " <> show id_)
-          Just name -> do
-            value <- getAttributeValue
-              version
-              (classAttributeMapObjectMap classAttributeMap)
-              name
-            pure (Attribute id_ name value)
+  -> DecodeBits Attribute
+getAttribute version classes actors actor = do
+  attributes <- lookupAttributeMap classes actors actor
+  limit <- lookupAttributeIdLimit attributes actor
+  attribute <- getCompressedWord limit
+  name <- lookupAttributeName classes attributes attribute
+  Attribute attribute name
+    <$> getAttributeValue version (classAttributeMapObjectMap classes) name
+
+lookupAttributeMap
+  :: ClassAttributeMap
+  -> Map CompressedWord Word32le
+  -> CompressedWord
+  -> DecodeBits (Map Word32le Word32le)
+lookupAttributeMap classes actors actor = fromMaybe
+  ("could not get attribute map for " <> show actor)
+  (getAttributeMap classes actors actor)
+
+lookupAttributeIdLimit
+  :: Map Word32le Word32le -> CompressedWord -> DecodeBits Word
+lookupAttributeIdLimit attributes actor = fromMaybe
+  ("could not get attribute ID limit for " <> show actor)
+  (getAttributeIdLimit attributes)
+
+lookupAttributeName
+  :: ClassAttributeMap
+  -> Map Word32le Word32le
+  -> CompressedWord
+  -> DecodeBits Str
+lookupAttributeName classes attributes attribute = fromMaybe
+  ("could not get attribute name for " <> show attribute)
+  (getAttributeName classes attributes attribute)
+
+fromMaybe :: String -> Maybe a -> DecodeBits a
+fromMaybe message = maybe (fail message) pure
