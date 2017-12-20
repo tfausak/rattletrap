3b63ff0d96e566a67c281e58287e562179926a3a
Taylor Fausak <taylor@fausak.me>
2017-12-20 17:08:29 -0600

    Fix JSON encoding of dictionaries

--- library/Rattletrap/Type/Dictionary.hs
+++ library/Rattletrap/Type/Dictionary.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Rattletrap.Type.Dictionary
   ( Dictionary(..)
   , dictionaryKeys
@@ -10,14 +8,41 @@ module Rattletrap.Type.Dictionary
 import Rattletrap.Type.Common
 import Rattletrap.Type.Str
 
+import qualified Control.Monad as Monad
+import qualified Data.Aeson as Json
+import qualified Data.Aeson.Types as Json
 import qualified Data.Map as Map
+import qualified Data.Text as Text
 
 data Dictionary a
   = DictionaryElement Str a (Dictionary a)
   | DictionaryEnd Str
   deriving (Eq, Ord, Show)
 
-$(deriveJson ''Dictionary)
+instance Json.FromJSON a => Json.FromJSON (Dictionary a) where
+  parseJSON = Json.withObject "Dictionary" (\ o -> do
+    keys <- get o "keys"
+    lastKey <- get o "last_key"
+    value <- get o "value"
+    Monad.foldM
+      (\ d k -> case Map.lookup k value of
+        Nothing -> fail (unwords ["missing key", show k])
+        Just v -> pure (DictionaryElement (Str k) v d))
+      (DictionaryEnd lastKey)
+      (reverse keys))
+
+get :: Json.FromJSON a => Json.Object -> String -> Json.Parser a
+get o k = o Json..: Text.pack k
+
+instance Json.ToJSON a => Json.ToJSON (Dictionary a) where
+  toJSON d = Json.object
+    [ pair "keys" (dictionaryKeys d)
+    , pair "last_key" (dictionaryLastKey d)
+    , pair "value" (dictionaryValue d)
+    ]
+
+pair :: Json.ToJSON a => String -> a -> (Text, Json.Value)
+pair k v = (Text.pack k, Json.toJSON v)
 
 dictionaryKeys :: Dictionary a -> [Str]
 dictionaryKeys = fmap fst . toList
