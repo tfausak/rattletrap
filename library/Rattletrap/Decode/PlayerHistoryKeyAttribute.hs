1fc7d54346f5ff69c30dfb50f4d05a3c6dc2c1b2
Taylor Fausak <taylor@fausak.me>
2017-12-22 13:08:30 -0600

    Parse 14 bits for player history keys

--- library/Rattletrap/Decode/PlayerHistoryKeyAttribute.hs
+++ library/Rattletrap/Decode/PlayerHistoryKeyAttribute.hs
@@ -7,19 +7,6 @@ import Rattletrap.Type.PlayerHistoryKeyAttribute
 
 import qualified Control.Monad as Monad
 
-import System.Environment
-import System.IO.Unsafe
-import Text.Read
-
 decodePlayerHistoryKeyAttributeBits :: DecodeBits PlayerHistoryKeyAttribute
-decodePlayerHistoryKeyAttributeBits = PlayerHistoryKeyAttribute <$> Monad.replicateM numBits getBool
+decodePlayerHistoryKeyAttributeBits = PlayerHistoryKeyAttribute <$> Monad.replicateM 14 getBool
 
-numBits :: Int
-numBits = unsafePerformIO (do
-  m <- lookupEnv "RATTLETRAP_NUM_BITS"
-  case m of
-    Nothing -> pure 0
-    Just s -> case readMaybe s of
-      Nothing -> pure 0
-      Just n -> pure n)
-{-# NOINLINE numBits #-}
