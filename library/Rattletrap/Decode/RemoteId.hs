abc1aba76de6958356fa6b2fd31ff68cf80d2ac9
Taylor Fausak <taylor@fausak.me>
2019-05-24 21:18:10 -0400

    Handle new PsyNet IDs
    Fixes #115. Thanks @nickbabcock! https://github.com/nickbabcock/boxcars/pull/41

--- library/Rattletrap/Decode/RemoteId.hs
+++ library/Rattletrap/Decode/RemoteId.hs
@@ -23,7 +23,8 @@ decodeRemoteIdBits version systemId = case word8leValue systemId of
   2 -> RemoteIdPlayStation <$> decodePsName <*> decodePsBytes version
   4 -> RemoteIdXbox <$> decodeWord64leBits
   6 -> RemoteIdSwitch <$> decodeBitstreamBits 256
-  7 -> RemoteIdPsyNet <$> decodeBitstreamBits 256
+  7 -> RemoteIdPsyNet
+    <$> decodeBitstreamBits (if version >= (868, 24, 10) then 64 else 256)
   _ -> fail ("unknown system id " <> show systemId)
 
 decodePsName :: DecodeBits Text.Text
