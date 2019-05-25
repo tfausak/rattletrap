1b70f2c69eef051559f5cdab4f40737a5ca3fa29
Taylor Fausak <taylor@fausak.me>
2019-05-24 21:14:11 -0400

    Use the whole version for conditional
    This should have been done as part of 55550e4ad21f7bbc1205450c55d9195fa806287a.

--- library/Rattletrap/Decode/RemoteId.hs
+++ library/Rattletrap/Decode/RemoteId.hs
@@ -17,10 +17,10 @@ import qualified Data.Text.Encoding as Text
 import qualified Data.Word as Word
 
 decodeRemoteIdBits :: (Int, Int, Int) -> Word8le -> DecodeBits RemoteId
-decodeRemoteIdBits (_, _, patch) systemId = case word8leValue systemId of
+decodeRemoteIdBits version systemId = case word8leValue systemId of
   0 -> RemoteIdSplitscreen <$> getWord32be 24
   1 -> RemoteIdSteam <$> decodeWord64leBits
-  2 -> RemoteIdPlayStation <$> decodePsName <*> decodePsBytes patch
+  2 -> RemoteIdPlayStation <$> decodePsName <*> decodePsBytes version
   4 -> RemoteIdXbox <$> decodeWord64leBits
   6 -> RemoteIdSwitch <$> decodeBitstreamBits 256
   7 -> RemoteIdPsyNet <$> decodeBitstreamBits 256
@@ -31,6 +31,6 @@ decodePsName = fmap
   (Text.dropWhileEnd (== '\x00') . Text.decodeLatin1 . reverseBytes)
   (getByteStringBits 16)
 
-decodePsBytes :: Int -> DecodeBits [Word.Word8]
-decodePsBytes patch =
-  Bytes.unpack <$> getByteStringBits (if patch >= 1 then 24 else 16)
+decodePsBytes :: (Int, Int, Int) -> DecodeBits [Word.Word8]
+decodePsBytes version = Bytes.unpack
+  <$> getByteStringBits (if version >= (868, 20, 1) then 24 else 16)
