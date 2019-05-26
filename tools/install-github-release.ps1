36a66d9127a580c3b212dd47fef4369fcbdf2400
Taylor Fausak <taylor@fausak.me>
2019-05-26 10:00:55 -0400

    Upgrade to github-release 1.2.4

--- tools/install-github-release.ps1
+++ tools/install-github-release.ps1
@@ -1,5 +1,5 @@
 if (Test-Path c:/bin/github-release.exe) {} else {
-  curl -OutFile github-release.zip -Uri https://github.com/tfausak/github-release/releases/download/1.2.1/github-release-windows.zip
+  curl -OutFile github-release.zip -Uri https://github.com/tfausak/github-release/releases/download/1.2.4/github-release-windows.zip
   7z x github-release.zip github-release.exe
   mkdir c:/bin
   mv github-release.exe c:/bin
