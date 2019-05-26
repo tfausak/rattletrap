626453cff22099c1773447c155881845b1418102
Taylor Fausak <taylor@fausak.me>
2019-05-26 10:01:55 -0400

    Upgrade to Stack 1.9.3

--- tools/install-stack.ps1
+++ tools/install-stack.ps1
@@ -1,5 +1,5 @@
 if (Test-Path c:/bin/stack.exe) {} else {
-  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v1.9.1/stack-1.9.1-windows-x86_64.zip
+  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-windows-x86_64.zip
   7z x stack.zip stack.exe
   mkdir c:/bin
   mv stack.exe c:/bin
--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -3,7 +3,7 @@ set -o errexit -o xtrace
 
 if test ! -f "$HOME/.local/bin/stack"
 then
-  URL="https://github.com/commercialhaskell/stack/releases/download/v1.9.1/stack-1.9.1-$TRAVIS_OS_NAME-x86_64.tar.gz"
+  URL="https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-$TRAVIS_OS_NAME-x86_64.tar.gz"
   curl --location "$URL" --output stack.tar.gz
   gunzip stack.tar.gz
   tar -x -f stack.tar --strip-components 1
