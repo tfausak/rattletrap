1334eea060e615596f75967872988d4bead8d9e5
Taylor Fausak <taylor@fausak.me>
2019-05-26 11:05:11 -0400

    Add comments to suspicious looking types

--- library/Rattletrap/Type/GameModeAttribute.hs
+++ library/Rattletrap/Type/GameModeAttribute.hs
@@ -9,6 +9,10 @@ import Rattletrap.Type.Common
 
 data GameModeAttribute = GameModeAttribute
   { gameModeAttributeNumBits :: Int
+  -- ^ This field is guaranteed to be small. In other words, it won't overflow.
+  -- It's stored as a regular 'Int' rather than something more precise like an
+  -- 'Int8' because it just gets passed to functions that expect 'Int's.
+  -- There's no reason to do a bunch of conversions.
   , gameModeAttributeWord :: Word8
   } deriving (Eq, Ord, Show)
 
--- library/Rattletrap/Type/Vector.hs
+++ library/Rattletrap/Type/Vector.hs
@@ -11,9 +11,16 @@ import Rattletrap.Type.CompressedWord
 data Vector = Vector
   { vectorSize :: CompressedWord
   , vectorBias :: Word
+  -- ^ This field is guaranteed to be small. In other words, it won't overflow.
+  -- It's stored as a regular 'Word' rather than something more precise like a
+  -- 'Word8' because it just gets passed to a functions that expect 'Word's.
+  -- There's no reason to do a bunch of conversions.
   , vectorX :: Int
+  -- ^ See 'vectorBias'.
   , vectorY :: Int
+  -- ^ See 'vectorBias'.
   , vectorZ :: Int
+  -- ^ See 'vectorBias'.
   } deriving (Eq, Ord, Show)
 
 $(deriveJson ''Vector)
