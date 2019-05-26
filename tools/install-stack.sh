de7d8c1aecc8ac1e3927a236231c75a68e12acc7
Taylor Fausak <taylor@fausak.me>
2019-05-26 10:20:43 -0400

    Temporarily ignore cached executables

--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 set -o errexit -o xtrace
 
-if test ! -f "$HOME/.local/bin/stack"
-then
   URL="https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-$TRAVIS_OS_NAME-x86_64.tar.gz"
   curl --location "$URL" --output stack.tar.gz
   gunzip stack.tar.gz
@@ -10,6 +8,5 @@ then
   mkdir -p "$HOME/.local/bin"
   mv stack "$HOME/.local/bin/"
   rm stack.tar
-fi
 
 stack --version
