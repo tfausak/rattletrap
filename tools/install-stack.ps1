f3767f39165601d8b38a961788f71df3e8b7ec84
Taylor Fausak <taylor@fausak.me>
2020-04-04 22:54:32 -0400

    Upgrade to Stack 2.3.0.1

--- tools/install-stack.ps1
+++ tools/install-stack.ps1
@@ -1,8 +1,8 @@
-if (Test-Path c:/bin/stack.exe) {} else {
-  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-windows-x86_64.zip
+# if (Test-Path c:/bin/stack.exe) {} else {
+  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v2.3.0.1/stack-2.3.0.1-windows-x86_64.zip
   7z x stack.zip stack.exe
   mkdir c:/bin
   mv stack.exe c:/bin
-}
+# }
 
 stack --version
--- tools/install-stack.sh
+++ tools/install-stack.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 set -o errexit -o xtrace
 
-if test ! -f "$HOME/.local/bin/stack"
-then
-  URL="https://github.com/commercialhaskell/stack/releases/download/v1.9.3/stack-1.9.3-$TRAVIS_OS_NAME-x86_64.tar.gz"
+# if test ! -f "$HOME/.local/bin/stack"
+# then
+  URL="https://github.com/commercialhaskell/stack/releases/download/v2.3.0.1/stack-2.3.0.1-$TRAVIS_OS_NAME-x86_64.tar.gz"
   curl --location "$URL" --output stack.tar.gz
   gunzip stack.tar.gz
   tar -x -f stack.tar --strip-components 1
   mkdir -p "$HOME/.local/bin"
   mv stack "$HOME/.local/bin/"
   rm stack.tar
-fi
+# fi
 
 stack --version
