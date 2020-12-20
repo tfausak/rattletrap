195850ebdc6aca60281e719e4e0715e55443e6ba
Taylor Fausak <taylor@fausak.me>
2020-12-19 20:26:52 -0500

    Upload executable as build artifact

---
+++ tools/copy-executable.hs
@@ -0,0 +1,23 @@
+module Main ( main ) where
+import qualified Data.Char as Char
+import qualified Data.List as List
+import qualified System.Directory as Directory
+import qualified System.Process as Process
+
+main :: IO ()
+main = do
+  before <- Process.readProcess "which" [ "rattletrap" ] ""
+  print before
+  let after = convert $ List.dropWhileEnd Char.isSpace before
+  print after
+  Directory.copyFile after "output/rattletrap"
+
+convert :: FilePath -> FilePath
+convert path = case path of
+  '/' : drive : '/' : rest ->
+    Char.toUpper drive
+    : ':'
+    : '\\'
+    : fmap (\ x -> if x == '/' then '\\' else x) rest
+    <> ".exe"
+  _ -> path
