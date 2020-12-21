5338e5dab365133a9f2d5adbea49743ae69aaa38
Taylor Fausak <taylor@fausak.me>
2020-12-21 18:27:19 -0500

    Improve script for copying executables

--- src/util/copy-executable.hs
+++ src/util/copy-executable.hs
@@ -2,23 +2,30 @@ module Main ( main ) where
 import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified System.Directory as Directory
+import qualified System.FilePath as FilePath
 import qualified System.Process as Process
 
 main :: IO ()
 main = do
-  putStrLn "Copying executable ..."
-  before <- Process.readProcess "which" [ "rattletrap" ] ""
-  print before
-  let after = convert $ List.dropWhileEnd Char.isSpace before
-  print after
-  Directory.copyFile after "output/rattletrap"
+  original <- Process.readProcess "which" [ "rattletrap" ] ""
+  let
+    source = convert $ List.dropWhileEnd Char.isSpace original
+    directory = "output"
+    target = FilePath.combine directory "rattletrap"
+  putStrLn $ mconcat
+    [ "Copying "
+    , show source
+    , " ("
+    , show original
+    , ") to "
+    , show target
+    , " ..."
+    ]
+  Directory.createDirectoryIfMissing True directory
+  Directory.copyFile source target
 
 convert :: FilePath -> FilePath
 convert path = case path of
   '/' : drive : '/' : rest ->
-    Char.toUpper drive
-    : ':'
-    : '\\'
-    : fmap (\ x -> if x == '/' then '\\' else x) rest
-    <> ".exe"
+    Char.toUpper drive : ':' : '/' :  rest <> ".exe"
   _ -> path
