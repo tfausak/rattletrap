d153470101aad05e34d284077c8cc9a05a44c6e3
Taylor Fausak <taylor@fausak.me>
2020-12-20 22:17:40 -0500

    Disable AppVeyor

--- tools/install-stack.ps1
+++
@@ -1,8 +0,0 @@
-if (Test-Path c:/bin/stack.exe) {} else {
-  curl -OutFile stack.zip -Uri https://github.com/commercialhaskell/stack/releases/download/v2.3.1/stack-2.3.1-windows-x86_64.zip
-  7z x stack.zip stack.exe
-  mkdir c:/bin
-  mv stack.exe c:/bin
-}
-
-stack --version
