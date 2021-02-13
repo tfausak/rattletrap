0ca937ec354d7e69f5af011bda88085efb26b836
Taylor Fausak <taylor@fausak.me>
2021-02-13 10:14:32 -0500

    Don't accept JSON numbers for 64-bit values

--- src/lib/Rattletrap/Type/Int64le.hs
+++ src/lib/Rattletrap/Type/Int64le.hs
@@ -8,9 +8,7 @@ import qualified Data.Binary.Bits.Put as BinaryBits
 import qualified Data.Binary.Put as Binary
 import qualified Data.ByteString.Lazy as LazyBytes
 import qualified Data.Aeson as Aeson
-import qualified Data.Aeson.Types as Aeson
 import qualified Data.Int as Int
-import qualified Data.Scientific as Scientific
 import qualified Data.Text as Text
 import qualified Text.Read as Read
 
@@ -19,14 +17,8 @@ newtype Int64le = Int64le
   } deriving (Eq, Show)
 
 instance Aeson.FromJSON Int64le where
-  parseJSON value = case value of
-    Aeson.String text -> case Read.readEither $ Text.unpack text of
-      Left _ -> Aeson.typeMismatch "Int64le" value
-      Right int64 -> pure $ Int64le int64
-    Aeson.Number number -> case Scientific.toBoundedInteger number of
-      Nothing -> Aeson.typeMismatch "Int64le" value
-      Just int64 -> pure $ Int64le int64
-    _ -> Aeson.typeMismatch "Int64le" value
+  parseJSON = Aeson.withText "Int64le" $
+    either fail (pure . Int64le) . Read.readEither . Text.unpack
 
 instance Aeson.ToJSON Int64le where
   toJSON = Aeson.toJSON . show . int64leValue
