3597cc837bf9a22f6cbfb2614ddfacd0275c4078
Taylor Fausak <taylor@fausak.me>
2021-02-14 12:49:04 -0500

    Improve representation of loadouts

--- src/lib/Rattletrap/Type/Attribute/LoadoutOnline.hs
+++ src/lib/Rattletrap/Type/Attribute/LoadoutOnline.hs
@@ -4,25 +4,25 @@ module Rattletrap.Type.Attribute.LoadoutOnline where
 
 import Rattletrap.Type.Common
 import qualified Rattletrap.Type.Attribute.Product as Product
+import qualified Rattletrap.Type.List as List
 import qualified Rattletrap.Type.U8 as U8
 import Rattletrap.Decode.Common
 import qualified Rattletrap.Type.Str as Str
 import qualified Rattletrap.Type.U32 as U32
 import Rattletrap.Encode.Common
 
-import qualified Control.Monad as Monad
 import qualified Data.Map as Map
 
 newtype LoadoutOnline = LoadoutOnline
-  { value :: [[Product.Product]]
+  { value :: List.List (List.List Product.Product)
   } deriving (Eq, Show)
 
 $(deriveJson ''LoadoutOnline)
 
 bitPut :: LoadoutOnline -> BitPut ()
 bitPut loadoutAttribute = do
-  let attributes = value loadoutAttribute
-  U8.bitPut (U8.fromWord8 (fromIntegral (length attributes)))
+  let attributes = List.toArray $ value loadoutAttribute
+  U8.bitPut . U8.fromWord8 . fromIntegral $ length attributes
   mapM_ Product.putProductAttributes attributes
 
 bitGet
@@ -31,6 +31,6 @@ bitGet
   -> BitGet LoadoutOnline
 bitGet version objectMap = do
   size <- U8.bitGet
-  LoadoutOnline <$> Monad.replicateM
+  LoadoutOnline <$> List.replicateM
     (fromIntegral (U8.toWord8 size))
     (Product.decodeProductAttributesBits version objectMap)
--- src/lib/Rattletrap/Type/Attribute/Product.hs
+++ src/lib/Rattletrap/Type/Attribute/Product.hs
@@ -3,6 +3,7 @@
 module Rattletrap.Type.Attribute.Product where
 
 import Rattletrap.Type.Common
+import qualified Rattletrap.Type.List as List
 import qualified Rattletrap.Type.Str as Str
 import qualified Rattletrap.Type.U32 as U32
 import qualified Rattletrap.Type.U8 as U8
@@ -10,7 +11,6 @@ import qualified Rattletrap.Type.Attribute.ProductValue as ProductValue
 import Rattletrap.Decode.Common
 import Rattletrap.Encode.Common
 
-import qualified Control.Monad as Monad
 import qualified Data.Map as Map
 import qualified Data.Binary.Bits.Put as BinaryBits
 
@@ -25,10 +25,10 @@ data Product = Product
 
 $(deriveJson ''Product)
 
-putProductAttributes :: [Product] -> BitPut ()
+putProductAttributes :: List.List Product -> BitPut ()
 putProductAttributes attributes = do
-  U8.bitPut . U8.fromWord8 . fromIntegral $ length attributes
-  mapM_ bitPut attributes
+  U8.bitPut . U8.fromWord8 . fromIntegral . length $ List.toArray attributes
+  mapM_ bitPut $ List.toArray attributes
 
 bitPut :: Product -> BitPut ()
 bitPut attribute = do
@@ -37,12 +37,10 @@ bitPut attribute = do
   ProductValue.bitPut $ value attribute
 
 decodeProductAttributesBits
-  :: (Int, Int, Int) -> Map U32.U32 Str.Str -> BitGet [Product]
+  :: (Int, Int, Int) -> Map U32.U32 Str.Str -> BitGet (List.List Product)
 decodeProductAttributesBits version objectMap = do
   size <- U8.bitGet
-  Monad.replicateM
-    (fromIntegral $ U8.toWord8 size)
-    (bitGet version objectMap)
+  List.replicateM (fromIntegral $ U8.toWord8 size) $ bitGet version objectMap
 
 bitGet
   :: (Int, Int, Int) -> Map U32.U32 Str.Str -> BitGet Product
