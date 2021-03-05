fe495802affcb214c22815e4afc6a9bbc74eff0e
Taylor Fausak <taylor@fausak.me>
2021-03-04 20:41:53 -0500

    Clean up artifact creation

--- src/util/copy-executable.hs
+++ src/util/copy-executable.hs
@@ -5,15 +5,16 @@ module Main
 import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified System.Directory as Directory
+import qualified System.Environment as Environment
 import qualified System.FilePath as FilePath
 import qualified System.Process as Process
 
 main :: IO ()
 main = do
+  [directory] <- Environment.getArgs
   original <- Process.readProcess "which" ["rattletrap"] ""
   let
     source = convert $ List.dropWhileEnd Char.isSpace original
-    directory = "docker"
     target = FilePath.combine directory "rattletrap"
   putStrLn $ mconcat
     [ "Copying "
