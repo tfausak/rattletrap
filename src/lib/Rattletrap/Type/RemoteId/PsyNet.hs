c3831360a6c8732e70da64c5b579cac5bd8edda4
Taylor Fausak <taylor@fausak.me>
2021-09-19 23:12:27 +0000

    Avoid wrapping `PsyNet` around `Either`

--- src/lib/Rattletrap/Type/RemoteId/PsyNet.hs
+++ src/lib/Rattletrap/Type/RemoteId/PsyNet.hs
@@ -1,5 +1,6 @@
 module Rattletrap.Type.RemoteId.PsyNet where
 
+import qualified Control.Applicative as Applicative
 import qualified Rattletrap.BitGet as BitGet
 import qualified Rattletrap.BitPut as BitPut
 import qualified Rattletrap.Schema as Schema
@@ -7,21 +8,24 @@ import qualified Rattletrap.Type.U64 as U64
 import qualified Rattletrap.Type.Version as Version
 import qualified Rattletrap.Utility.Json as Json
 
-newtype PsyNet
-  = PsyNet (Either U64.U64 (U64.U64, U64.U64, U64.U64, U64.U64))
+data PsyNet
+  = New U64.U64
+  | Old U64.U64 U64.U64 U64.U64 U64.U64
   deriving (Eq, Show)
 
 instance Json.FromJSON PsyNet where
-  parseJSON = fmap fromEither . Json.parseJSON
+  parseJSON = Json.withObject "PsyNet" $ \ object -> do
+    let
+      new = fmap New $ Json.required object "Left"
+      old = do
+        (a, b, c, d) <- Json.required object "Right"
+        pure $ Old a b c d
+    new Applicative.<|> old
 
 instance Json.ToJSON PsyNet where
-  toJSON = Json.toJSON . toEither
-
-fromEither :: Either U64.U64 (U64.U64, U64.U64, U64.U64, U64.U64) -> PsyNet
-fromEither = PsyNet
-
-toEither :: PsyNet -> Either U64.U64 (U64.U64, U64.U64, U64.U64, U64.U64)
-toEither (PsyNet x) = x
+  toJSON x = case x of
+    New a -> Json.object [Json.pair "Left" a]
+    Old a b c d -> Json.object [Json.pair "Right" (a, b, c, d)]
 
 schema :: Schema.Schema
 schema = Schema.named "remote-id-psy-net" $ Schema.oneOf
@@ -34,20 +38,19 @@ schema = Schema.named "remote-id-psy-net" $ Schema.oneOf
   ]
 
 bitPut :: PsyNet -> BitPut.BitPut
-bitPut x = case toEither x of
-  Left l -> U64.bitPut l
-  Right (a, b, c, d) ->
+bitPut x = case x of
+  New l -> U64.bitPut l
+  Old a b c d ->
     U64.bitPut a <> U64.bitPut b <> U64.bitPut c <> U64.bitPut d
 
 bitGet :: Version.Version -> BitGet.BitGet PsyNet
 bitGet version =
   BitGet.label "PsyNet"
-    . fmap fromEither
     $ if Version.atLeast 868 24 10 version
-        then BitGet.label "new" $ fmap Left U64.bitGet
-        else BitGet.label "old" . fmap Right $ do
+        then BitGet.label "New" $ fmap New U64.bitGet
+        else BitGet.label "Old" $ do
           a <- U64.bitGet
           b <- U64.bitGet
           c <- U64.bitGet
           d <- U64.bitGet
-          pure (a, b, c, d)
+          pure $ Old a b c d
