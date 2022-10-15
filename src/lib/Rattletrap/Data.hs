92c06a4e977633d1334364698ee152a8995a1f6b
Taylor Fausak <taylor@fausak.me>
2022-10-15 07:58:00 -0500

    Handle new match GUID type

--- src/lib/Rattletrap/Data.hs
+++ src/lib/Rattletrap/Data.hs
@@ -273,6 +273,7 @@ attributeTypes = Map.fromList $ fmap
   , ("Engine.TeamInfo:Score", AttributeType.Int)
   , ("ProjectX.GRI_X:bGameStarted", AttributeType.Boolean)
   , ("ProjectX.GRI_X:GameServerID", AttributeType.QWord)
+  , ("ProjectX.GRI_X:MatchGuid", AttributeType.String)
   , ("ProjectX.GRI_X:MatchGUID", AttributeType.String)
   , ("ProjectX.GRI_X:ReplicatedGameMutatorIndex", AttributeType.Int)
   , ("ProjectX.GRI_X:ReplicatedGamePlaylist", AttributeType.Int)
