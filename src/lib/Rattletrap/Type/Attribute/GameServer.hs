057fd593493f07f375cfbc494ca2c8dacc594cb8
Taylor Fausak <taylor@fausak.me>
2022-12-15 03:38:03 +0000

    Handle new game server IDs

--- src/lib/Rattletrap/Type/Attribute/GameServer.hs
+++ src/lib/Rattletrap/Type/Attribute/GameServer.hs
@@ -1,5 +1,6 @@
 module Rattletrap.Type.Attribute.GameServer where
 
+import qualified Data.Foldable as Foldable
 import qualified Rattletrap.BitGet as BitGet
 import qualified Rattletrap.BitPut as BitPut
 import qualified Rattletrap.Schema as Schema
@@ -7,21 +8,36 @@ import qualified Rattletrap.Type.Attribute.QWord as QWord
 import qualified Rattletrap.Type.Str as Str
 import qualified Rattletrap.Utility.Json as Json
 
-newtype GameServer
-  = GameServer QWord.QWord
+data GameServer
+  = Old QWord.QWord
+  | New Str.Str
   deriving (Eq, Show)
 
 instance Json.FromJSON GameServer where
-  parseJSON = fmap GameServer . Json.parseJSON
+  parseJSON = Json.withObject "GameServer" $ \x -> Foldable.asum
+    [ fmap Old $ Json.required x "old"
+    , fmap New $ Json.required x "new"
+    ]
 
 instance Json.ToJSON GameServer where
-  toJSON (GameServer x) = Json.toJSON x
+  toJSON x = case x of
+    Old y -> Json.object [Json.pair "old" $ Json.toJSON y]
+    New y -> Json.object [Json.pair "new" $ Json.toJSON y]
 
 schema :: Schema.Schema
-schema = Schema.named "attribute-game-server" $ Schema.ref QWord.schema
+schema = Schema.named "attribute-game-server" . Schema.oneOf $ fmap
+  (\(k, v) -> Schema.object [(Json.pair k v, True)])
+  [ ("old", Schema.ref QWord.schema)
+  , ("new", Schema.ref Str.schema)
+  ]
 
 bitPut :: GameServer -> BitPut.BitPut
-bitPut (GameServer x) = QWord.bitPut x
+bitPut x = case x of
+  Old y -> QWord.bitPut y
+  New y -> Str.bitPut y
 
 bitGet :: Maybe Str.Str -> BitGet.BitGet GameServer
-bitGet buildVersion = BitGet.label "GameServer" $ fmap GameServer QWord.bitGet
+bitGet buildVersion = BitGet.label "GameServer" $
+  if buildVersion >= Just (Str.fromString "221120.42953.406184")
+    then BitGet.label "New" $ fmap New Str.bitGet
+    else BitGet.label "Old" $ fmap Old QWord.bitGet
